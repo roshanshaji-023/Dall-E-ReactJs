e5d55ac8deef779395d0fe00dd3fe0d3771fa42a
roshan <roshanshaji023@gmail.com>
2023-01-06 22:39:13 +0530

    modifications

--- openaiapp/src/App.jsx
+++ openaiapp/src/App.jsx
@@ -61,7 +61,10 @@ function App() {
         </>
       )}
       {
-        <h6>Made as a learn-by-do project ! <a href="https://github.com/roshanshaji-023/Dall-E-ReactJs/">View Source Code</a></h6>
+        <>
+        <h6>Made as a learn-by-do project ! <a href="https://github.com/roshanshaji-023/Dall-E-ReactJs/">View Source Code</a></h6><br />
+       
+        </>
       }
     </div>
   );
